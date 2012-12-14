941cb94ac21025fc8949d9f2370fe160c1698140
fredo-dedup <frederic.testard@gmail.com>
2012-12-14 19:32:53 +0100

    almost there

--- src/SimpleMCMC.jl
+++ src/SimpleMCMC.jl
@@ -1,12 +1,277 @@
-
-module SimpleMCMC
+#module SimpleMCMC
 
 	# using Base
-	# using String
 
 	export simpleRWM, expexp, parseExpr
+	export unfoldBlock,
+	localVars,
+	backwardSweep,
+	derive,
+	unfoldExpr
 
-	####################################################################################
+##########  unfoldBlock ##############
+
+	function unfoldBlock(ex::Expr)
+		assert(ex.head == :block, "[unfoldBlock] not a block")
+
+		lb = {}  # will store expressions
+		vars = {} # will store variables in the block
+		for e in ex.args  #  e  = ex.args[8]
+			if e.head == :line   # line number marker, no treatment
+				push(lb, e)
+			elseif e.head == :(=)  # assigment
+				lhs = e.args[1]
+				if typeof(lhs) == Symbol # simple var case
+					push(vars, lhs)
+				elseif typeof(lhs) == Expr && lhs.head == :ref  # vars with []
+					push(vars, lhs.args[1])
+				else
+					error("[unfoldBlock] not a symbol on LHS of assigment $(e)") 
+				end
+
+				rhs = e.args[2]
+				if typeof(rhs) == Symbol ||  (typeof(rhs) == Expr && rhs.head == :ref)
+					push(lb, e)
+				elseif typeof(rhs) == Expr
+					ue = unfoldExpr(rhs)
+					if length(ue)==1 # simple expression, no variable creation necessary
+						push(lb, e)
+					else # several nested expressions
+						for a in ue[1:end-1]
+							push(lb, a)
+							lhs2 = a.args[1]
+							if typeof(lhs2) == Symbol # simple var case
+								push(vars, lhs2)
+							elseif typeof(lhs2) == Expr && lhs2.head == :ref  # vars with []
+								push(vars, lhs2.args[1])
+							else
+								error("[unfoldBlock] not a symbol on LHS of assigment $(e)") 
+							end
+						end
+						push(lb, :($lhs = $(ue[end])))
+					end
+				else  # unmanaged kind of lhs
+				 	error("[unfoldBlock] can't parse $e")
+				end
+
+			elseif e.head == :for  # TODO parse loops
+
+			elseif e.head == :while  # TODO parse loops
+
+			elseif e.head == :if  # TODO parse if structures
+
+			elseif e.head == :block
+				(e2, nv) = unfoldBlock(e)
+				map(x->push(lb,x), e2)  # exprs are inserted in parent block without begin - end
+				map(x->push(vars,x), nv)
+			else
+				error("[unfoldBlock] can't handle $(e.head) expressions")
+			end
+		end
+
+		(Expr(:block, lb, Any), vars)
+	end
+
+	function localVars(ex::Expr)
+		assert(ex.head == :block, "[localVars] not a block")
+
+		lb = {}
+		for e in ex.args  #  e  = ex.args[2]
+			if e.head == :(=)  # assigment
+				assert(typeof(e.args[1]) == Symbol, "[localVars] not a symbol on LHS of assigment $(e)") # TODO : manage symbol[]
+				
+				contains(lb, e.args[1]) ? nothing : push(lb, e.args[1])
+
+			elseif e.head == :line  # TODO parse loops
+
+			elseif e.head == :for  # TODO parse loops
+
+			elseif e.head == :while  # TODO parse loops
+
+			elseif e.head == :if  # TODO parse if structures
+
+			elseif e.head == :block
+				e2 = localVars(e)
+				for v in e2
+					push(lb, v)
+				end
+			else
+				error("[localVars] can't handle $(e.head) expressions")
+			end
+		end
+
+		lb
+	end
+
+##########  backwardSweep ##############
+
+	function backwardSweep(ex::Expr, locals::Vector)
+		assert(ex.head == :block, "[backwardSweep] not a block")
+
+		lb = {}
+		for e in ex.args  #  e  = ex.args[2]
+			if e.head == :line   # line number marker, no treatment
+				push(lb, e)
+			elseif e.head == :(=)  # assigment
+				lhs = e.args[1]
+				if typeof(lhs) == Symbol # simple var case
+					dsym = lhs
+					dsymref = ""
+				elseif typeof(lhs) == Expr && lhs.head == :ref  # vars with []
+					dsym = lhs.args[1]
+					dsymref = "[$(lhs.args[2])]"
+				else
+					error("[backwardSweep] not a symbol on LHS of assigment $(e)") 
+				end
+				
+				rhs = e.args[2]
+				if typeof(rhs) == Expr
+					println
+					for i in 2:length(rhs.args)
+						vsym = rhs.args[i]
+						if contains(locals, vsym)
+							push(lb, derive(rhs, i-1, :($dsym$dsymref)))
+						end
+					end
+				elseif typeof(rhs) == Symbol
+					dsym2 = symbol("__d$dsym$dsymref")
+					vsym2 = symbol("__d$rhs")
+					push(lb, :( $vsym2 = $dsym2) )
+				else 
+				end
+
+			elseif e.head == :for  # TODO parse loops
+				# e2 = backwardSweep(e)
+				# push(lb, e2)
+
+			elseif e.head == :while  # TODO parse loops
+				# e2 = backwardSweep(e)
+				# push(lb, e2)
+
+			elseif e.head == :if  # TODO parse if structures
+				# e2 = backwardSweep(e.args[2])
+				# push(lb, e2)
+
+			elseif e.head == :block
+				e2 = backwardSweep(e, locals)
+				push(lb, e2)
+			else
+				error("[backwardSweep] can't handle $(e.head) expressions")
+			end
+		end
+
+		Expr(:block, reverse(lb), Any)
+	end
+
+	function derive(opex::Expr, index::Integer, dsym::Expr)
+		op = opex.args[1]  # operator
+		vsym = opex.args[1+index]
+		vsym2 = symbol("__d$vsym")
+		dsym2 = symbol("__d$dsym")
+
+		if op == :+ 
+			return :($vsym2 += $dsym2)
+
+		elseif op == :- 
+			if length(opex.args) == 2  # unary minus
+				return :($vsym2 += -$dsym2)
+			elseif index == 1
+				return :($vsym2 += $dsym2)
+			else
+				return :($vsym2 += -$dsym2)
+			end
+
+		elseif op == :^
+			if index == 1
+				e = opex.args[3]
+				if e == 2.0
+					return :($vsym2 += 2 * $vsym * $dsym2)
+				else
+					return :($vsym2 += $e * $vsym ^ $(e-1) * $dsym2)
+				end
+			else
+				v = opex.args[2]
+				return :($vsym2 += log($v) * $v ^ $vsym * $dsym2)
+			end
+
+		elseif op == :*
+			e = :(1.0)
+			if index == 1
+				return :($vsym2 += $dsym2 * transpose($(opex.args[2])))
+			else
+				return :($vsym2 += transpose($(opex.args[2])) * $dsym2)
+			end
+
+		elseif op == :dot
+			e = index == 1 ? opex.args[3] : opex.args[2]
+			return :($vsym2 += sum($e) .* $dsym2)
+
+		elseif op == :log
+			return :($vsym2 += $dsym2 ./ $vsym)
+
+		elseif op == :exp
+			return :($vsym2 += exp($vsym) .* $dsym2)
+
+		elseif op == :/
+			if index == 1
+				e = opex.args[3]
+				return :($vsym2 += $vsym ./ $e .* $dsym2)
+			else
+				v = opex.args[2]
+				return :($vsym2 += - $v ./ ($vsym .* $vsym) .* $dsym2)
+			end
+		else
+			error("[derive] Doesn't know how to derive operator $op")
+		end
+	end
+
+##########  unfold Expr ##############
+	function unfoldExpr(ex::Expr)
+		assert(typeof(ex) == Expr, "[unfoldExpr] not an Expr $(ex)")
+		
+		if ex.head == :ref  # var[], send back directly
+			return {ex}		
+		elseif ex.head == :call
+			lb = {}
+			na = {ex.args[1]}   # function name
+			ex2 = ex.args[2:end]
+
+			# if more than 2 arguments, convert to nested expressions (easier for derivation)
+			# TODO : probably not valid for all ternary, quaternary, etc.. operators, should work for *, +, sum
+			while length(ex2) > 2
+				a2 = pop(ex2)
+				a1 = pop(ex2)
+				push(ex2, Expr(:call, {ex.args[1], a1, a2}, Any))
+			end
+
+			for e in ex2  # e = :b
+				if typeof(e) == Expr
+					ue = unfoldExpr(e)
+					for a in ue[1:end-1]
+						push(lb, a)
+					end
+					nv = consume(nameTask)
+					push(lb, :($nv = $(ue[end])))
+					push(na, nv)
+				else
+					push(na, e)
+				end
+			end
+			push(lb, Expr(ex.head, na, Any))
+			return lb
+		else
+			error("[unfoldExpr] cannot handle $(ex)")
+		end
+	end
+
+##########  var name factory   ##############
+	function nameFactory()
+		for i in 1:10000
+			produce(symbol("__t$i"))
+		end
+	end
+
+##########  helper function  to analyse expressions   #############
 
 	function expexp(ex::Expr, ident...)
 		ident = (length(ident)==0 ? 0 : ident[1])::Integer
@@ -92,6 +357,8 @@ module SimpleMCMC
 	#####################################################################
 
 	function parseExpr(ex::Expr, assigns::Array{Any}, index::Integer)
+		println(ex.head)
+
 		# ex = model ; assigns = {} ; index = 1
 		block = {}
 		for e in ex.args  # e = ex.args[4]
@@ -103,7 +370,7 @@ module SimpleMCMC
 				par = e.args[1]  # param symbol defined here
 
 				if e.args[2] == :scalar  #  simple decl : var::scalar
-					push(assigns, :($(par) = beta[$(index+1)]))
+					push(assigns, :($(par) = __beta[$(index+1)]))
 					index += 1
 
 				elseif typeof(e.args[2]) == Expr && e.args[2].head == :call
@@ -123,7 +390,7 @@ module SimpleMCMC
 				end
 			
 			elseif e.head == :call && e.args[1] == :~
-				e = :(acc = acc + sum(logpdf($(e.args[3]), $(e.args[2]))))
+				e = :(__acc = _acc + sum(logpdf($(e.args[3]), $(e.args[2]))))
 				# println("++++", e)
 				push(block, e)
 			else 
@@ -146,4 +413,4 @@ module SimpleMCMC
 
 	parseExpr(ex::Expr) = parseExpr(ex, {}, 0) # initial function call
 
-end  # end of module
+# end  # end of module
---
+++ src/scrapbook5.jl
@@ -0,0 +1,180 @@
+load("SimpleMCMC.jl")
+
+expexp(model2)
+expexp(:(a+b+c))
+expexp(:(sum(a,b,c)))
+expexp(:(if a==b ; c=d; elseif a<b ; c=2d; else c=3d; end))
+expexp(:(for i in 1:10; b=3.0;end))
+expexp(:(while d > 4; d+= 1;end))
+
+
+##########  tests ##############
+@assert isequal(unfoldExpr(:(3 + x)), [:(3+x)])
+@assert isequal(unfoldExpr(:(3*b + x)), [:(__t1 = 3*b), :(__t1 + x)])
+
+
+nameTask = Task(nameFactory)
+# consume(nameTask)
+
+ex=quote
+	a[12] = b
+	c = beta[3]
+	d[j] = ll[k:(k+5)]
+	z = sin(beta) / exp(a[45]) * c ^ (d + 5 +k)
+end
+(ex2, vars) = unfoldBlock(ex)
+
+ex=quote
+	c = beta[2:10]
+	a = b * c
+end
+(ex2, vars) = unfoldBlock(ex)
+backwardSweep(ex2, vars)
+
+
+#############################################
+ex = quote
+	b::scalar
+	ll::vector(10)
+
+	a[12] = b
+	c = beta[3]
+	d[j] = ll[k:(k+5)]
+	z = sin(beta) / exp(a[45]) * c ^ (d + 5 +k)
+	z ~ Normal(0,1)
+end
+(ex2, param_map, nparams) = parseExpr(ex)
+(ex3, vars) = unfoldBlock(ex2)
+push(vars, :__beta)
+ex4 = backwardSweep(ex3, vars)
+
+ex = quote
+	a = __beta[12]
+end
+(ex2, param_map, nparams) = parseExpr(ex)
+(ex3, vars) = unfoldBlock(ex2)
+push(vars, :__beta)
+ex4 = backwardSweep(ex3, vars)
+
+
+
+ex = quote
+	a = 5 + beta1
+	y = 7beta2 + log(a)
+	z = a * y
+end
+ex2 = unfoldBlock(ex)
+vars = localVars(ex2)
+push(vars, :beta1)
+push(vars, :beta2)
+ex3 = backwardSweep(ex2, vars)
+
+finalexp = quote
+	$ex2
+	$(Expr(:block, {:($(symbol("__d$v")) = 0.0) for v in vars}, Any))  
+	__dz = 1.0
+	# y, __dz, __da, __dt9, __dt10 = 0.0, 1.0, 0.0, 0.0, 0.0
+	$ex3
+	(z, __dbeta1, __dbeta2)
+end
+
+beta1, beta2 = 1.0, 3.0
+eval(finalexp)
+# (136.75055681536833,23.791759469228055,42.0)
+
+eval(ex) #136
+beta1, beta2 = 1.1, 3.0
+(eval(ex) - 136.75) * 10 # 23.80 ok
+beta1, beta2 = 1.0, 3.1
+(eval(ex) - 136.75) * 10 # 42.005 ok
+
+ref_time = timefactor(ex, 1000000)  # 0.26 sec
+timefactor(finalexp, 1000000, ref_time) # x3.5
+
+##############################################################
+begin
+	srand(1)
+	n = 100
+	nbeta = 4
+	X = [fill(1, (n,)) randn((n, nbeta-1))]
+	beta0 = randn((nbeta,))
+	Y = (1/(1+exp(-(X * beta0)))) .> rand(n)
+end
+
+model = quote
+	ll = - dot(beta, beta)
+	resid = Y - (1/(1+exp(- X * beta)))
+	ll = ll - dot(resid, resid)
+end
+
+ex2 = unfoldBlock(model)
+vars = localVars(ex2)
+push(vars, :beta)
+ex3 = backwardSweep(ex2, vars)
+
+finalexp = quote
+	$ex2
+	$(Expr(:block, {:($(symbol("__d$v")) = zero($(symbol("$v")))) for v in vars}, Any))  
+#	$(Expr(:block, {:($(symbol("__d$v")) = 0.0) for v in vars}, Any))  
+	__dll = 1.0
+    __d__t27 += __dll
+    __dll += __dll
+    __dresid += .*(sum(resid), __d__t27)
+    __dresid += .*(sum(resid), __d__t27)	#  line 4:
+    __d__t26 += -(__dresid)
+    __d__t25 += .*(./(-(1), .*(__t25, __t25)), __d__t26)
+    __d__t24 += __d__t25
+    __d__t23 += .*(exp(__t23), __d__t24)
+    __dbeta += *(*(1.0, __t22'), __d__t23)
+    __d__t22 += *(__d__t23, *(beta, 1.0)')	#  line 3: #'
+    __d__t21 += -(__dll)
+    __dbeta += .*(sum(beta), __d__t21)
+    __dbeta += .*(sum(beta), __d__t21)	#  line 2:
+	(ll, __dbeta)
+end
+
+beta = ones(4)
+eval(finalexp)
+eval(ex2)
+
+function HMC(finalexp::Expr, epsilon, L, current_beta)
+	beta = current_beta
+	p = randn(nbeta) # independent standard normal variates
+	current_beta = p
+
+	(ll, dll) = eval(finalexp)	
+	# Make a half step for momentum at the beginning
+	p -= epsilon * dll / 2
+	# Alternate full steps for position and momentum
+	for (i in 1:L)
+		# Make a full step for the position
+		beta += epsilon * p
+		# Make a full step for the momentum, except at end of trajectory
+		if i!=L
+			p -= epsilon * dll
+		end
+	end
+	# Make a half step for momentum at the end.
+	p -= epsilon * dll / 2
+	# Negate momentum at end of trajectory to make the proposal symmetric
+	p = -p
+	# Evaluate potential and kinetic energies at start and end of trajectory
+
+	(ll, dll) = eval(finalexp)	
+	current_U = U(current_q)
+	current_K = sum(current_p^2) / 2
+	proposed_U = U(q)
+	proposed_K = sum(p^2) / 2
+	# Accept or reject the state at end of trajectory, returning either
+	# the position at the end of the trajectory or the initial position
+	if (rand(1) < exp(current_U-proposed_U+current_K-proposed_K))
+	{
+	return (q) # accept
+	}
+	else
+	{
+	return (current_q) # reject
+	}
+}
+
+
